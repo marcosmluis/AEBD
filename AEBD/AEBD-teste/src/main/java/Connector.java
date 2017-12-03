d168ed7e58704f04ec91b0ddd919a13e91d5dc23
sn <sn@192.168.1.82>
2017-12-03 17:27:05 +0000

    Testes de inserção na BD

--- AEBD/AEBD-teste/src/main/java/Connector.java
+++ AEBD/AEBD-teste/src/main/java/Connector.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.lang.String ; 
 
 /**
  *
@@ -20,6 +21,7 @@ public class Connector  {
     
 public static void main(String[] args){
  
+    Connection conn1 = null;
     Connection conn2 = null;
     
     try{
@@ -29,10 +31,24 @@ public static void main(String[] args){
             String dbURL1 = "jdbc:oracle:thin:@//localhost:1521/orcl" ;
             String username = "sys as SYSDBA";
             String password = "oracle";
-            conn2 = DriverManager.getConnection(dbURL1, username, password);
+            
+            conn1 = DriverManager.getConnection(dbURL1, username, password);
+            
+            String dbURL2 = "jdbc:oracle:thin:@//localhost:1521/orcl" ;
+            String username2 = "Nelson";
+            String password2 = "pass";
+            
+            conn2 = DriverManager.getConnection(dbURL2, username2, password2);
+            
+            if (conn1 != null) {
+                System.out.println("Connected with connection #1");
+            }
+            else{
+                System.out.println(".....") ; 
+            }
             
             if (conn2 != null) {
-                System.out.println("Connected with connection #1");
+                System.out.println("Connected with connection #2");
             }
             else{
                 System.out.println(".....") ; 
@@ -40,7 +56,7 @@ public static void main(String[] args){
             
             
             String query = " Select * FROM user_tables ";
-            Statement stmt = conn2.createStatement();
+            Statement stmt = conn1.createStatement();
             ResultSet resultSet = stmt.executeQuery(query);
             /**
             System.out.println("Printing TABLE_TYPE \"TABLE\" ");
@@ -56,12 +72,21 @@ public static void main(String[] args){
             resultSet = stmt.executeQuery(query);
             System.out.println("Printing Tablespaces \"TABLESPACE\" ");
             System.out.println("----------------------------------");
+            int i = 1 ; 
             while(resultSet.next())
             {
                 //Print
-                System.out.println(resultSet.getString("tablespace_name"));
+                
+                System.out.println(resultSet.getString("tablespace_name"));                
                 }
+                Statement st = conn2.createStatement() ; 
+                String name = "BBBB" ; 
+                String s = "INSERT INTO TABLESPACE " + "VALUES ("+ i++ +","+ name + ", 'ONLINE' , 0 , 0 ,NULL)" ; 
+                System.out.println(s) ; 
+                System.out.println(st.executeUpdate(s)) ;
             
+            
+            /**
             query = "SELECT * FROM V$DATAFILE" ; 
             resultSet = stmt.executeQuery(query);
             System.out.println("Printing DATAFILE_NAME \"DATAFILE\" ");
@@ -91,7 +116,7 @@ public static void main(String[] args){
                 //Print
                 System.out.println(resultSet.getString("SID") + "   " + resultSet.getString("USERNAME") );
                 }
-            
+            */
             
             
            
